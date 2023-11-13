2a8a41461949ddc5cd395bd40bc610ae7cde68ae
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-11-13 00:27:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import React from "react";
-import Form from "./Form.js";
-import City from "./City";
-import Time from "./Time";
-import Description from "./Description";
-import Humidity from "./Humidity";
-import Wind from "./Wind";
-import Temperature from "./Temperature";
-import Forecast from "./Forecast";
-
+import Weather from "./Weather.js";
 import { Link } from "react-router-dom";
 
 import "./App.css";
@@ -16,31 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Form />
-        <City />
-        <div class="row" id="weather-attributes">
-          <div class="col-6">
-            <ul>
-              <li>
-                <Time />
-                <Description />
-              </li>
-              <Humidity />
-              <Wind />
-            </ul>
-          </div>
-          <div class="col-6">
-            <div class="d-flex weather-temperature">
-              <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-                alt="Clear"
-                id="icon"
-              />
-              <Temperature />
-            </div>
-          </div>
-        </div>
-        <Forecast />
+        <Weather />
       </div>
       <div className="link">
         <p>
