fea0ca50d9eff35e4dd6ea54b49aa32c727eb555
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-11-23 00:14:38 +0000

    Update WeatherDate.js

--- src/WeatherDate.js
+++ src/WeatherDate.js
@@ -1,5 +1,31 @@
 import React from "react";
 
-export default function weatherDate(props) {
-  return <p>Thursday </p>;
+export default function WeatherDate(props) {
+  console.log(props.date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
 }
