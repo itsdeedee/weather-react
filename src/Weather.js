758160ad6cb96f1939646e2af0f927573271a415
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-12-01 20:24:39 +0000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,9 @@ import Weather2 from "./Weather2";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  const [weatherTime, setWeatherTime] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: Math.round(response.data.temperature.current),
@@ -17,21 +15,8 @@ export default function Weather(props) {
       city: response.data.city,
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
-      time: response.data.time,
+      time: new Date(response.data.time * 1000),
     });
-  }
-  function search() {
-    const apiKey = "bdc1aa3oa00cd461t2421e4af03336bc";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    const ApiKey = "46bcf398c3089fd0db0296fd3c3fb1f6";
-    let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
-    axios.get(ApiUrl).then(handleTime);
-  }
-  function handleTime(response) {
-    console.log(response.data);
-    setWeatherTime({ date: new Date(response.data.dt * 1000) });
   }
 
   function handleSubmit(event) {
@@ -40,6 +25,11 @@ export default function Weather(props) {
   }
   function handleCityChange(event) {
     setCity(event.target.value);
+  }
+  function search() {
+    const apiKey = "bdc1aa3oa00cd461t2421e4af03336bc";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
   if (weatherData.ready) {
     return (
@@ -55,17 +45,17 @@ export default function Weather(props) {
                 onChange={handleCityChange}
               />
             </div>
-            <div class="col-2">
+            <div className="col-2">
               <input
                 type="submit"
                 value="Search"
-                class="form-control btn btn-outline-info"
+                className="form-control btn btn-outline-info"
               />
             </div>
-            <div class="col-3">
+            <div className="col-3">
               <button
                 type="button"
-                class="btn btn-outline-info"
+                className="btn btn-outline-info"
                 id="currentButton"
               >
                 Current
@@ -73,7 +63,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <Weather2 data={weatherData} time={weatherTime} />
+        <Weather2 data={weatherData} />
         <Forecast />
       </div>
     );
