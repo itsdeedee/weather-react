cbb28252ee5ee094a2a64e2a42894040387788b1
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-12-03 23:47:19 +0000

    Create ForecastDay.js

---
+++ src/ForecastDay.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import WeatherForecastDay from "./Forecast";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "bdc1aa3oa00cd461t2421e4af03336bc";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
