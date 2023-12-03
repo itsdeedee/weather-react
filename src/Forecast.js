58a2a76107864442affce7306b995c7f3716bb31
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-12-03 23:47:14 +0000

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,83 +1,36 @@
 import React from "react";
 
-export default function Forecast() {
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
-    <div className="Forecast">
-      <div class="weather-forecast" id="forecast">
-        <div class="row">
-          <div class="col-2">
-            <div class="weather-forecast">Mon</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-          <div class="col-2">
-            <div class="weather-forecast">Tue</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-          <div class="col-2">
-            <div class="weather-forecast">Wed</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-          <div class="col-2">
-            <div class="weather-forecast">Thur</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-          <div class="col-2">
-            <div class="weather-forecast">Fri</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-          <div class="col-2">
-            <div class="weather-forecast">Sat</div>
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
-              alt="clear"
-              width="42"
-            />
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">12°</span>
-              <span class="weather-forecast-temperature-min"> 3° </span>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div className="WeatherForecast-day">{day()}</div>
+      <img src={props.data.iconUrl} alt="Clear" id="icon" />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperature-min">
+          {minTemperature()}
+        </span>
       </div>
     </div>
   );
